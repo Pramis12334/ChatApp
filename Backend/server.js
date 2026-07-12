844f9e3663588538f6e7b32cdd1749322bb685d8
Pramis Raya <hemanta.rayamajhi2@gmail.com>
2026-07-12 13:49:22 +0545

    Database connect

--- Backend/server.js
+++ Backend/server.js
@@ -5,3 +5,4 @@ const app = require('./src/server');
 app.listen(3000, () => {
 console.log("Server is running on port 3000");
 });
+
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const route = require('./routes/server');
+const { connectDB } = require('./lib/db');
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use('/api/auth', route.Authroute);
