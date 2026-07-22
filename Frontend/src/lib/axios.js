fe3fb788d1fe77a252312b66a564dd62fc15e6aa
Pramis Raya <hemanta.rayamajhi2@gmail.com>
2026-07-22 12:21:23 +0545

    created client side authentication functionality

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseUrl: "http://localhost:3000/api",
+    baseURL: "http://localhost:3000/api",
     withCredentials: true
 });
