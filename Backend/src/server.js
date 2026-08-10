0e6d4d9471be79d47fd4c7fabedf58f6ade7b8c3
Pramis Raya <hemanta.rayamajhi2@gmail.com>
2026-08-10 12:02:09 +0545

    rate limiting and bot detection using arcjet

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,12 +3,14 @@ import route from './routes/server.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app } from './lib/socket.io.js';
+import {arcjetProtection} from './middlewares/arcjet.middlewares.js';
 
 
 app.use(express.json());
 app.use(cors({origin:process.env.CLIENT_URL, credentials: true}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(arcjetProtection);
 app.use('/api/auth', route.Authroute);
 app.use('/api/message', route.Messageroute);
 
